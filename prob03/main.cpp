0a8838939aa72d4ef92d19072109ed8987532d46
stephanietalley <stephanietalley@cs104u-5b.acad.fullerton.edu>
2019-09-21 14:32:52 -0700

    Created random rectanle generator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,25 @@
 // Random Rectangle
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+
+//Defined the max possible length of the rectangle as being 40
+  const int MAX_LENGTH = 40;
+  std::string eq;
+  
+//Assigns the random numbers
+  unsigned seed = time (0);
+  srand (seed);
+
+//Set equation to finding a random number for the rectangle's length
+  int num = rand() % MAX_LENGTH + 1;
+  eq.assign (num, '#');
+
+  std::cout << eq << std::endl;
+  std::cout << eq << std::endl;
+  std::cout << eq << std::endl;
+
+  return 0;
+}
