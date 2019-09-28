697523e386398854ea3fefab0b616bfab87f0019
stephanietalley <stephanietalley@cs104u-4h.acad.fullerton.edu>
2019-09-28 13:31:55 -0700

    Answered all TODO's

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -28,6 +28,9 @@ int main()
   f2 *= 0.01;
 
   // TODO#1 add comment describing what's happening
+  /* By continuously checking the sizes of the two values as the value of the
+  8-byte is retained due to the double having a more precise value than that
+  of the floating-point.*/
   std::cout << "\nThe 8 byte manage to keep its value, however the 4 byte" << std::endl;
   std::cout << "became slightly off and thus lost its precision." << std::endl;
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
@@ -37,6 +40,9 @@ int main()
   f2 *= 0.1;
 
   // TODO#2 add comment describing what's happening
+  /* Despite being set to the same exact value, the value set as a floating-point
+  could no longer retain it's original value, thus leaving the double to have
+  more precision*/
   std::cout << "\nThe 8 byte was still able to maintain its value, however the" << std::endl;
   std::cout << "4 byte could no longer maintain the precision of the value." << std::endl;
   std::cout << sizeof(d2) << " bytes, value: " << d2 << std::endl;
@@ -46,6 +52,9 @@ int main()
   float f3 = 3.5E38;
 
   // TODO#3 add comment describing what happened
+  /* The floating-point, being much too large at this point, goes on to infinity with
+  no end value, resulting in an error message, whereas the 8-byte value set to double
+  retained it's original value*/
   std::cout << "\nThe value for the 4 byte was much too large that it resulted" << std::endl;
   std::cout << "in an error of infinite." << std::endl;
   std::cout << std::setprecision(1);
